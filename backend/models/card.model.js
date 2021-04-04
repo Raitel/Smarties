889cfcf96cb13a5f755efebd8a1b1c26ccd7ff90
Raitel <raitel@qq.com>
2021-04-04 07:12:10 -0400

    card schema fix

--- backend/models/card.model.js
+++ backend/models/card.model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cardSchema = new Schema({
-  type: { type: String },
-  price: { type: Number },
-  path: { type: String }
+  type: { type: String, required: true  },
+  price: { type: Number, required: true  },
+  path: { type: String, required: true  }
 }, {
   timestamps: true,
 });
