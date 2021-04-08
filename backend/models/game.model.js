c99df6dc94a972f204620ad5605d08d8c4a44213
Raitel <raitel@qq.com>
2021-04-08 06:14:17 -0400

    nested Stages inside game, addStage function

--- backend/models/game.model.js
+++ backend/models/game.model.js
@@ -8,6 +8,20 @@ const defaultQuestionCard = "606e7b59bee1d0599ca713b7";
 const defaultTipCard = "606e7b59bee1d0599ca713b7";
 const defaultAnswerCard = "606e7b59bee1d0599ca713b7";
 
+const nestedStageSchema = new Schema({
+  type: { type: String},
+  question: { type: String, default: ""},
+  answer: { type: String, default: ""},
+  tip1: { type: String, default: ""},
+  tip2: { type: String, default: ""},
+  choice1: { type: String, default: ""},
+  choice2: { type: String, default: ""},
+  choice3: { type: String, default: ""},
+  choice4: { type: String, default: ""},
+  choice5: { type: String, default: ""},
+  letters: [String]
+});
+
 const gameSchema = new Schema({
   title: {type: String, required: true},
   description: {type: String, default: "", trim: true},
@@ -16,10 +30,15 @@ const gameSchema = new Schema({
   questionCard: {type: Schema.Types.ObjectId, default: defaultQuestionCard, ref: 'Card'},
   tipCard: {type: Schema.Types.ObjectId, default: defaultTipCard, ref: 'Card'},
   answerCard: {type: Schema.Types.ObjectId, default: defaultAnswerCard, ref: 'Card'},
+  nestedStages:[nestedStageSchema]
 }, {
   timestamps: true,
 });
 
+
+
 const Game = mongoose.model('Game', gameSchema);
+const nestedStages = mongoose.model('nestedStages', nestedStageSchema);
 
 module.exports = Game;
+//module.exports = nestedStages;
--- backend/routes/games.js
+++ backend/routes/games.js
@@ -3,13 +3,13 @@ let Game = require('../models/game.model');
 
 router.route('/').get((req, res) => {
     Game.find()
-    .then(cards => res.json(cards))
+    .then(games => res.json(games))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/:id').get((req, res) => {
     Game.findById(req.params.id)
-    .then(card => res.json(card))
+    .then(game => res.json(game))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
@@ -20,7 +20,7 @@ router.route('/add').post((req, res) => {
   const stages = req.body.stages;
   const questionCard = req.body.questionCard;
   const tipCard = req.body.tipCards;
-  const answerCard = req.body.AnswerCard;
+  const answerCard = req.body.answerCard;
 
   const newGame = new Game({
     title,
@@ -59,7 +59,7 @@ router.route('/update/:id').post((req, res) => {
         if(req.body.stages){retrievedGame.stages = req.body.stages};
         if(req.body.questionCard){retrievedGame.questionCard = req.body.questionCard};
         if(req.body.tipCard){retrievedGame.tipCard = req.body.tipCard};
-        if(req.body.answerCard){retrievedGame.AnswerCard = req.body.answerCard};
+        if(req.body.answerCard){retrievedGame.answerCard = req.body.answerCard};
         retrievedGame.save(function(err, updatedGame) {
           if (err){
             console.log(err);
@@ -73,5 +73,76 @@ router.route('/update/:id').post((req, res) => {
   });
 });
 
+// router.route('/updateStages/:id').post((req, res) => {
+//   Game.findById(req.params.id, function(err, retrievedGame){
+//   if(err){
+//     console.log(err);
+//     res.status(500).send()
+//   }else{
+//     if(!retrievedGame){
+//       res.status(404).send()
+//     } 
+//     else{
+//       if(req.body.type){retrievedStage.type = req.body.type};
+//       if(req.body.question){retrievedStage.question = req.body.question};
+//       if(req.body.answer){retrievedStage.answer = req.body.answer};
+//       if(req.body.path){retrievedStage.path = req.body.path};
+//       if(req.body.tip1){retrievedStage.tip1 = req.body.tip1};
+//       if(req.body.tip2){retrievedStage.tip2 = req.body.tip2};
+//       if(req.body.choice1){retrievedStage.choice1 = req.body.choice1};
+//       if(req.body.choice2){retrievedStage.choice2 = req.body.choice2};
+//       if(req.body.choice3){retrievedStage.choice3 = req.body.choice3};
+//       if(req.body.choice4){retrievedStage.choice4 = req.body.choice4};
+//       if(req.body.choice5){retrievedStage.choice5 = req.body.choice5};
+//       if(req.body.letters){retrievedStage.letters = req.body.letters};
 
+//       retrievedGame.nestedStages.push({type: 'testNestedStage'});
+//       retrievedGame.save(function(err, updatedGame) {
+//         if (err){
+//           console.log(err);
+//           res.status(500).send()
+//         }else{
+//           res.send(updatedGame)
+//         };
+//       });
+//     };
+//   };
+// });
+// });
+
+router.route('/addStage/:id').post((req, res) => {
+  Game.findById(req.params.id, function(err, retrievedGame){
+  if(err){
+    console.log(err);
+    res.status(500).send()
+  }else{
+    if(!retrievedGame){
+      res.status(404).send()
+    } 
+    else{
+      retrievedGame.nestedStages.push({
+        type: req.body.type,
+        question: req.body.question,
+        answer: req.body.answer,
+        tip1: req.body.tip1,
+        tip2: req.body.tip2,
+        choice1: req.body.choice1,
+        choice2: req.body.choice2,
+        choice3: req.body.choice3,
+        choice4: req.body.choice4,
+        choice5: req.body.choice5,
+        letters: req.body.letters
+    });
+      retrievedGame.save(function(err, updatedGame) {
+        if (err){
+          console.log(err);
+          res.status(500).send()
+        }else{
+          res.send(updatedGame)
+        };
+      });
+    };
+  };
+});
+});
 module.exports = router;
