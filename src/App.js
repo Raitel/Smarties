d27071693e91305b1b53e2d1f01f6d9b6c874e5c
Louis C <lchoicer57@gmail.com>
2021-04-14 17:01:16 -0400

    platform mapping

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from "./components/profile";
 import Favorites from "./components/favorites";
 import Mygames from "./components/mygames";
 import Inventory from "./components/inventory";
+import ContactUs from "./components/contact.js";
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
       <Route path="/favorites" exact component={Favorites} />
       <Route path="/mygames" exact component={Mygames} />
       <Route path="/inventory" exact component={Inventory} />
-
+      <Route path="/contactus" exact component={ContactUs} />
     </Router>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -3,13 +3,75 @@ import { Link } from 'react-router-dom';
 
 import LeftPanel from "./leftpanel";
 
+function Populate(props){
+    const items = props.items;
+    const listItems = items.map((item) => 
+        <div>{item.title}</div>
+    );
+    return (
+        <div>{listItems}</div>
+    );
+}
+
 export default class Home extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            recentlyUsed: [
+                {
+                    "description": "",
+                    "isPublic": true,
+                    "upvotes": 0,
+                    "downvotes": 0,
+                    "tags": [],
+                    "games": [
+                        "606542da7084a516a96bc8f1",
+                        "606542da7084a516a96bc8f2"
+                    ],
+                    "_id": "606ea28cf3b2dc0d08e40adc",
+                    "ownerid": "606542da7084a516a96bc8f1",
+                    "title": "dummyTest",
+                    "createdAt": "2021-04-08T06:28:28.237Z",
+                    "updatedAt": "2021-04-09T21:42:49.920Z",
+                    "__v": 2
+                },
+                {
+                    "description": "",
+                    "isPublic": true,
+                    "upvotes": 0,
+                    "downvotes": 0,
+                    "tags": [],
+                    "games": [],
+                    "_id": "606f8d1d7195479521336568",
+                    "ownerid": "606f8bc2ea8d56647dfaaeff",
+                    "title": "Louis' Second Platform",
+                    "createdAt": "2021-04-08T23:09:17.243Z",
+                    "updatedAt": "2021-04-08T23:09:17.243Z",
+                    "__v": 0
+                }
+            ],
+            facorites: [],
+            myGames: []
+        };
+      }
+    
     render(){
         return(
             <div style={{display:'flex'}}>
                 <LeftPanel></LeftPanel>
-                <div>
-                    home panel
+                <div style={{paddingTop:'110px', paddingLeft: '75px'}}>
+                    <div style={{height:'200px'}}>
+                        <b style={{color:'#212197', fontSize:'24px', }}>Recently Used:</b>
+                        <Populate items={this.state.recentlyUsed} />
+                    </div>
+                    <div style={{height:'200px'}}>
+                        <b style={{color:'#212197', fontSize:'24px', }}>Favorites:</b>
+                        <div></div>
+                    </div>
+                    <div style={{height:'200px'}}>
+                        <b style={{color:'#212197', fontSize:'24px', }}>My Games:</b>
+                        <div></div>
+                    </div>
                 </div>
             </div>
         )
