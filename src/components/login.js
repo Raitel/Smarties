3466a984e567943696184ccb2b311ed767b77528
Louis C <lchoicer57@gmail.com>
2021-04-15 12:55:51 -0400

    login and register

--- src/components/login.js
+++ src/components/login.js
@@ -1,149 +1,139 @@
 import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import Textfield from '@material-ui/core/Textfield';
-import { makeStyles } from '@material-ui/core/styles';
-import logo from '../assets/logo.png';
-import { Container } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import InputAdornment from "@material-ui/core/InputAdornment";
-import MailOutlineRoundedIcon from '@material-ui/icons/MailOutlineRounded';
+import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const useStyles = makeStyles({
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: '100px'
-    },
-    subcontainer: {
-        width: '400px',
-        height: '600px',
-        borderRadius:'25px',
-        border:'2px solid grey', 
-        display:'flex', 
-        flexDirection:'column', 
-        alignItems:'center',
-        paddingTop:'40px'
-    },
-    header:{
-        marginBottom:'10px', 
-        marginTop:'15px', 
-        display:'flex',
-    },
-    logo: {
-        width:'90px', 
-        height:'60px'
-    },
-    title:{
-        color:'#212197', 
-        fontSize:'30px', 
-        marginLeft:'-10px',
-        textAlign:'center',
-        lineHeight: '60px'
-    },
-    blurb:{
-        color:'#212197', 
-        fontSize:'16px',
-        marginBottom: '20px',
-        textAlign:'center'
-    },
-    formContents:{
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center',
-        width:'350px'
-    },
-    field: {
-        marginBottom: '20px'
-    },
-    misc:{
-        marginTop:'20px',
-        color:'#212197'
-    },
-    offset:{
-        textAlign:'right'
-    }
-  });
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit">
+        Smarties
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export default function Login(){
-    const classes = useStyles();
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [emailError, setEmailError] = useState(false)
-    const [passwordError, setPasswordError] = useState(false)
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setEmailError(false)
-        setPasswordError(false)
-        if(email == ''){
-            setEmailError(true)
-        }
-        if(password == ''){
-            setPasswordError(true)
-        }
-        if(email && password){
-            console.log(email, password)
-        }
-    }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-    return (
-        <Container className={classes.container}>
-            <Container className={classes.subcontainer}>
-                <Grid container justify="center" spacing={0} className={classes.header}>
-                    <img src={logo} alt="logo" className={classes.logo}/>
-                    <Box component="b" className={classes.title}>Smarties</Box>
-                </Grid>
-                <Container className={classes.blurb}>Log in with your Smarties Account</Container>
-                <form noValidate autoComplete="off" onSubmit={handleSubmit} className={classes.formContents}>
-                    <Textfield 
-                        onChange={(e) => setEmail(e.target.value)}
-                        label="Email" 
-                        variant="outlined" 
-                        fullWidth 
-                        required 
-                        className={classes.field}
-                        error={emailError}
-                        InputProps={{
-                            endAdornment:(
-                                <InputAdornment>
-                                    <MailOutlineRoundedIcon/>
-                                </InputAdornment>
-                            )
-                        }}
-                    />
-                    <Textfield 
-                        onChange={(e) => setPassword(e.target.value)}
-                        label="Password" 
-                        type="password"
-                        variant="outlined" 
-                        fullWidth 
-                        required 
-                        className={classes.field}
-                        error={passwordError}
-                        InputProps={{
-                            endAdornment:(
-                                <InputAdornment>
-                                    <LockOutlinedIcon/>
-                                </InputAdornment>
-                            )
-                        }}
-                    />
-                    <Button 
-                        variant="contained" 
-                        color="primary" 
-                        type="submit">
-                        Log in
-                    </Button>
-                </form>
-                <Grid container spacing={3} className={classes.misc}>
-                    <Grid item xs={6}>Forgot password?</Grid>
-                    <Grid item xs={6} className={classes.offset}>Sign-up now</Grid>
-                </Grid>
-                
-            </Container>
-        </Container>
-    )
+export default function Login() {
+  const classes = useStyles();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      setEmailError(false)
+      setPasswordError(false)
+      if(email == ''){
+          setEmailError(true)
+      }
+      if(password == ''){
+          setPasswordError(true)
+      }
+      if(email && password){
+          console.log(email, password)
+      }
+  }
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            onChange={(e) => setEmail(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            error={emailError}
+          />
+          <TextField
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            error={passwordError}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="http://localhost:3000/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
