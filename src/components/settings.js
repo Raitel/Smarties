780b8290295065222069865b0cbde984ae9ab7fb
Raitel <raitel@qq.com>
2021-04-16 08:17:48 -0400

    setting screen done without user icon

--- src/components/settings.js
+++ src/components/settings.js
@@ -10,65 +10,108 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Button from '@material-ui/core/Button';
-import Avatar from "@material-ui/core/Avatar";
+
 import Divider from '@material-ui/core/Divider';
 import Typography from "@material-ui/core/Typography";
 
-import Container from '@material-ui/core/Container';
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
+    root: {
+        flexGrow: 1,
+      },
+    card:{
+        width: "800px" 
     },
-  },
 }));
 
 export default function FormPropsTextFields() {
     const classes = useStyles();
   
         return(
-            <Grid>
-            <LeftPanel/>
-            <Container component="main" maxWidth="md">
-                <Card>
-                    <CardHeader title={"Account Settings:"} style={{color:'#212197', fontWeight: 'Bold'}}/>
-                    <CardContent>
-                        <Typography style={{color:'#212197', fontWeight: 'Bold'}}>
-                        Email:
-                        </Typography>
-                        <TextField
-                            disabled
-                            id="outlined-disabled"
-                            
-                            defaultValue="Hello World"
-                            variant="outlined"
-                            />
-                        <Divider />
-                        <Typography style={{color:'#212197', fontWeight: 'Bold'}}>
-                        Username:
-                        </Typography>
-                        <TextField
-                            disabled
-                            id="outlined-disabled"
-                            
-                            defaultValue="Hello World"
-                            variant="outlined"
-                            />
-                        <Button variant="contained" color="primary" >
-                            Change Username
-                        </Button>
-                        <Divider />
-                        <Typography style={{color:'#212197', fontWeight: 'Bold'}}>
-                        Password:
-                        </Typography>
+            <div style={{display:'flex', marginTop:"64px"}}>
+                <LeftPanel/>
+                <div>
+                    <Box p={3}>    
+                        <Card className={classes.card}>
+                            <Box p={2}>
+                            <CardHeader title={"Account Settings:"} style={{color:'#212197', fontWeight: 'Bold'}}/>
+                            </Box>
+                            <CardContent>
+                            <Box p={2}>
+                            <Grid container
+                            direction="column"
+                            justify="space-around"
+                            alignItems="flex-start"
+                            spacing={3}>
+                                <Grid item>
+                                <Typography style={{color:'#212197', fontWeight: 'Bold'}}>
+                                Email:
+                                </Typography>
+                                </Grid>
+                                <Grid item>
+                                <TextField
+                                    disabled
+                                    id="outlined-disabled"
+                                    
+                                    defaultValue="Test@smarties.com"
+                                    variant="outlined"
+                                    />
+                                </Grid>
+
+                                <Grid item>
+                                    <Divider />
+                                </Grid>
+
+                                <Grid item>
+                                <Typography style={{color:'#212197', fontWeight: 'Bold'}}>
+                                Username:
+                                </Typography>
+                                </Grid>
+
+                                
+                                <Grid item
+                                container
+                                direction="row"
+                                justify="space-between"
+                                alignItems="center"
+                                spacing={3}>
+                                    <Grid item>
+                                        <TextField
+                                        disabled
+                                        id="outlined-disabled"
+                                        defaultValue="ThisIsATempUsername"
+                                        variant="outlined"
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" color="primary" style={{textTransform: 'none'}} >
+                                            Change Username
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                                
+
+                                <Grid item>
+                                    <Divider />
+                                </Grid>
+                                <Grid item>
+                                    <Typography style={{color:'#212197', fontWeight: 'Bold'}}>
+                                        Password:
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="contained" color="primary" style={{textTransform: 'none'}}>
+                                        Change Password
+                                    </Button> 
+                                </Grid>
+                            </Grid>  
+                            </Box>               
+                        </CardContent>
+                    </Card>
+                </Box>
+
         
-                        <Button variant="contained" color="primary">
-                            Change Password
-                        </Button>                       
-                     </CardContent>
-                </Card>
-            </Container>
-        </Grid>
-        )
-    }
+                    
+             </div>
+        </div> 
+    )
+}
