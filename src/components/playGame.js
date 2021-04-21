671c3d5490ef518ed18e25254fbf1ae01ad0a2f8
Louis C <lchoicer57@gmail.com>
2021-04-21 13:51:47 -0400

    play game skeleton

--- src/components/playGame.js
+++ src/components/playGame.js
@@ -19,9 +19,12 @@ import Grid from '@material-ui/core/Grid';
 import { CardMedia, StylesProvider } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
-import stage from '../assets/game_assets/stage.png'
+import Stage from '../assets/game_assets/stage.png'
 import Question from '../assets/game_assets/questioncard600.png'
-import tip from '../assets/game_assets/tipcard.png'
+import Answer from '../assets/game_assets/answercard400.png'
+import Tip from '../assets/game_assets/tipcard.png'
+import ConstructBox from '../assets/game_assets/constructionBox.png'
+import Background from '../assets/game_assets/background.png'
 import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
@@ -29,6 +32,7 @@ const useStyles = makeStyles((theme) => ({
       maxWidth: 345
     },
     container:{
+        backgroundImage: `url(${Background})`,
         width:'1200px',
         height:'800px',
         marginTop:'100px',
@@ -37,143 +41,295 @@ const useStyles = makeStyles((theme) => ({
         flexDirection:'column',
         border: '1px solid black'
     },
-    overlay: {
-       position: 'absolute',
-       top: '20px',
-       left: '20px',
-       color: 'black',
-       backgroundColor: 'white'
-    },
-    stageRoot: {
-        minWidth: 600,
-        border: "none", 
-        boxShadow: "none" 
-    },
-    stageMedia:{
-        width:'100%',
-        height:'150px', // absolutely REQUIRED
-    },
-    stageOverlay:{
-        position: 'relative',
-        top: '-105px',
-        left: '500px',
-        color: 'black',
-    },
-    questioncontainer:{
-        display:'flex',
-        justifyContent:'center'
-    },
-    questionRoot:{
-        minWidth: 600,
-        height: 150,
-        border: "none", 
-        boxShadow: "none" 
-    },
-    questionMedia:{
-        width:'600px',
-        height:'130px', // absolutely REQUIRED
-    },
-    questionOverlay:{
-        position: 'relative',
-        color: 'black',
-        top: '-105px',
-        left: '500px',
-    },
-    paperContainerQuestion: {
+    questionPaper:{
         backgroundImage: `url(${Question})`,
         width:'600px',
         height:'130px',
-    }
+        display:'flex',
+        textAlign:'center',
+        alignItems:'center',
+        justifyContent:'center',
+        color:'white',
+        marginBottom:'30px'
+    },
+    tipCard1:{
+        backgroundImage: `url(${Tip})`,
+        width:'92px',
+        height:'135px',
+        display:'flex',
+        textAlign:'center',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    tipCard2:{
+        backgroundImage: `url(${Tip})`,
+        width:'93px',
+        height:'135px',
+        display:'flex',
+        textAlign:'center',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    stagePaper:{
+        backgroundImage: `url(${Stage})`,
+        width:'633px',
+        height:'118px',
+        display:'flex',
+        textAlign:'center',
+        alignItems:'center',
+        justifyContent:'center',
+        marginBottom:'30px'
+    },
+    stageHeader:{
+        color:'white',
+        position:'relative',
+        top:'-15px'
+    },
+    constructPaper:{
+        backgroundImage: `url(${Answer})`,
+        width:'400px',
+        height:'130px',
+        display:'flex',
+        textAlign:'center',
+        alignItems:'center',
+        justifyContent:'center',
+        color:'white'
+    },
+    letterRow:{
+        display:'flex'
+    },
+    letterPaper:{
+        backgroundImage: `url(${ConstructBox})`,
+        width:'92px',
+        height:'96px',
+        display:'flex',
+        textAlign:'center',
+        alignItems:'center',
+        justifyContent:'center',
+        fontSize:'24px',
+        color:'white'
+    },
+    rowFormat:{
+        display:'flex'
+    },
+    answerPaper:{
+        backgroundImage: `url(${Answer})`,
+        width:'400px',
+        height:'100px',
+        display:'flex',
+        textAlign:'center',
+        alignItems:'center',
+        justifyContent:'center',
+        color:'white'
+    },
   }));
 
 export default function PlayGame() {
     const classes = useStyles();
     const history = useHistory();
+    const [game, setGame] = useState(
+        {
+          "description": "",
+          "tags": ['best', 'game', 'class', 'school', 'study', 'college'],
+          "stages": [],
+          "questionCard": "606e7b59bee1d0599ca713b7",
+          "tipCard": "606e7b59bee1d0599ca713b7",
+          "answerCard": "606e7b59bee1d0599ca713b7",
+          "_id": "60708e459ca18d2708a34203",
+          "title": "first game update",
+          "nestedStages": [
+              {
+                  "question": "",
+                  "answer": "",
+                  "tip1": "",
+                  "tip2": "",
+                  "choice1": "",
+                  "choice2": "",
+                  "choice3": "",
+                  "choice4": "",
+                  "choice5": "",
+                  "letters": ['a','N','Q','R','h','W','o','K','J','U','V','B','M','j'],
+                  "_id": "6070a325ad0c350524e78ea9",
+                  "type": "CN"
+              },
+              {
+                  "question": "",
+                  "answer": "",
+                  "tip1": "",
+                  "tip2": "",
+                  "choice1": "",
+                  "choice2": "",
+                  "choice3": "",
+                  "choice4": "",
+                  "choice5": "",
+                  "letters": [],
+                  "_id": "6070aa21c2617208fc247dbe",
+                  "type": "TX"
+              }
+          ],
+          "createdAt": "2021-04-09T17:26:29.533Z",
+          "updatedAt": "2021-04-09T19:25:21.806Z",
+          "__v": 4
+        }
+    )
+
+    function GenerateRow(props){
+        var counter;
+        if (props.row == 1){
+            counter = 0
+        }else{
+            counter = 7
+        }
+        var rowItems = []
+        var limit = counter + 7
+        while(counter < limit){
+            rowItems.push(
+                <Container className={classes.letterPaper}>{game.nestedStages[0].letters[counter]}</Container>
+            )
+            counter++;
+        }
+
+        return (
+            [rowItems]
+        )
+    }
 
     function Construction(props){
-
         return(
-            <Container className={classes.container}>
-                <Container>
-                    <Card className={classes.stageRoot}>
-                        <CardMedia className={classes.stageMedia} image={stage} title='stage'/>
-                        <div className={classes.stageOverlay}>
-                            Stage 5/10
-                        </div>
-                    </Card>
-                </Container>
-                <Container className={classes.questioncontainer}>
-                    <Card className={classes.questionRoot}>
-                        <CardMedia className={classes.questionMedia} image={Question} title='question'/>
-                        <div className={classes.questionOverlay}>
-                            What is the best Game ever?
-                        </div>
-                    </Card>
-                </Container>
+            <Container>
+                <Container className={classes.container}>
 
-                Testing Paper
-                <Paper className={classes.paperContainerQuestion}>
-                    <Container>Some text to fill the Paper Component (Only Container)</Container>
-                </Paper>    
-                <Paper className={classes.paperContainerQuestion}>
-                    <Container>
-                        <Typography>
-                            Some text to fill the Paper Component (Typograph inside Container)
+                    <Container className={classes.stagePaper}>
+                        <Typography className={classes.stageHeader}>
+                            Stage Paper
                         </Typography>
                     </Container>
-                </Paper>
-                <Paper className={classes.paperContainerQuestion}>
-                    <Container>
-                        <Grid
-                        container
-                        direction="row"
-                        justify="center"
-                        alignItems="center"
-                        >
-                            <Grid item>
-                                <Typography>
-                                    Some text to fill the Paper Component(Typography inside Grid)
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Container>
-                </Paper>
-                End of Testing Paper    
 
-                <Grid container>
-                    <Grid item image={tip} > </Grid>
-                    <Grid item image={tip} > </Grid>
-                </Grid>
+                    <Container className={classes.questionPaper}>
+                        Question Paper
+                    </Container>
+
+                    <Grid container direction="row" justify="center" alignItems="center" spacing={10}>
+                        <Grid item>
+                            <Container className={classes.tipCard1}>
+                                Tip Card 1
+                            </Container>
+                        </Grid>
+                        <Grid item>
+                            <Container className={classes.tipCard2}>
+                                Tip Card 2
+                            </Container>
+                        </Grid>
+                    </Grid>
+
+                    <Container className={classes.constructPaper}>
+                        Construct Paper
+                    </Container>
+
+                    <Container className={classes.letterRow}>
+                        <GenerateRow row={1}/>
+                    </Container>
+                    <Container className={classes.letterRow}>
+                        <GenerateRow row={2}/>
+                    </Container>
+                    
+                </Container>
             </Container>
-            
         )
     }
     function Multiple(props){
         return(
-            <Container className={classes.container}>
-                <Container>Banner</Container>
-                <Container>Question</Container>
-                <Container>Tip Cards</Container>
-                <Container>User Input</Container>
-                <Container>Letters</Container>
+            <Container>
+                <Container className={classes.container}>
+
+                    <Container className={classes.stagePaper}>
+                        <Typography className={classes.stageHeader}>
+                            Stage Paper
+                        </Typography>
+                    </Container>
+
+                    <Container className={classes.questionPaper}>
+                        Question Paper
+                    </Container>
+
+                    <Grid container direction="column" justify="center" alignItems="center" >
+                        <Grid container style={{marginBottom:'20px'}}>
+                            <Container className={classes.constructPaper}>
+                                Construct Paper
+                            </Container>
+                            <Grid item>
+                                <Container className={classes.tipCard1}>
+                                    Tip Card 1
+                                </Container>
+                            </Grid>
+                            <Container className={classes.constructPaper}>
+                                Construct Paper
+                            </Container>
+                        </Grid>
+                        <Grid container style={{marginBottom:'20px'}}>
+                            <Container className={classes.constructPaper}>
+                                Construct Paper
+                            </Container>
+                        </Grid>
+                        <Grid container style={{marginBottom:'20px'}}>
+                            <Container className={classes.constructPaper}>
+                                Construct Paper
+                            </Container>
+                            <Grid item>
+                                <Container className={classes.tipCard1}>
+                                    Tip Card 1
+                                </Container>
+                            </Grid>
+                            <Container className={classes.constructPaper}>
+                                Construct Paper
+                            </Container>
+                        </Grid>
+                    </Grid>
+                    
+                </Container>
             </Container>
         )
     }
     function Text(props){
         return(
-            <Container className={classes.container}>
-                <Container>Banner</Container>
-                <Container>Question</Container>
-                <Container>Tip Cards</Container>
-                <Container>User Input</Container>
-                <Container>Letters</Container>
+            <Container>
+                <Container className={classes.container}>
+
+                    <Container className={classes.stagePaper}>
+                        <Typography className={classes.stageHeader}>
+                            Stage Paper
+                        </Typography>
+                    </Container>
+
+                    <Container className={classes.questionPaper}>
+                        Question Paper
+                    </Container>
+
+                    <Grid container direction="column" justify="center" alignItems="center" spacing={2} style={{marginBottom:'15px'}}>
+                        <Grid item>
+                            <Container className={classes.tipCard1}>
+                                Tip Card 1
+                            </Container>
+                        </Grid>
+                        <Grid item>
+                            <Container className={classes.tipCard2}>
+                                Tip Card 2
+                            </Container>
+                        </Grid>
+                    </Grid>
+
+                    <Container className={classes.constructPaper}>
+                        Construct Paper
+                    </Container>
+                    
+                </Container>
             </Container>
         )
     }
     return(
     <div style={{display:'flex', marginTop:"64px"}}>
         <LeftGamePanel/>
-        <Construction/>
+        <Multiple/>
     </div>
 
     )
