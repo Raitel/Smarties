ffabfd88cb8654aae41e6971f28806f3a954091c
Raitel He <raitel@qq.com>
2025-02-06 16:53:59 -0500

    Create craco.config.js

---
+++ client/craco.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        stream: require.resolve("stream-browserify")
+      };
+      return webpackConfig;
+    }
+  }
+};
